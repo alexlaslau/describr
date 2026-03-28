09336e83f3eeafe5013398f49e0a43d0ca63de9b
Alex <george_laslau@yahoo.com>
2026-03-28 22:24:31 +0200

    feat: adds caching for fetching count of products on dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 
 class User extends Authenticatable
 {
@@ -51,10 +52,12 @@ class User extends Authenticatable
 
     public function getProductStats(): array
     {
-        return [
-            'total' => $this->products()->count(),
-            'completed' => $this->products()->completed()->count(),
-            'inProgress' => $this->products()->inProgress()->count(),
-        ];
+        return Cache::remember("user:{$this->id}:product-stats", now()->addMinutes(5), function (): array {
+            return [
+                'total' => $this->products()->count(),
+                'completed' => $this->products()->completed()->count(),
+                'inProgress' => $this->products()->inProgress()->count(),
+            ];
+        });
     }
 }
